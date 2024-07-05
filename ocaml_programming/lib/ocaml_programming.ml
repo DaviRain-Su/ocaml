17a52c127e7d0a610d53095b7c80668103b48186
Davirain <davirain.yin@gmail.com>
2024-07-05 13:39:50 +0800

    add length and empty

--- ocaml_programming/lib/ocaml_programming.ml
+++ ocaml_programming/lib/ocaml_programming.ml
@@ -2,6 +2,23 @@ let rec sum = function
   | [] -> 0 (* wrong! *)
   | h::t -> h + sum t
 
+let rec length = function
+  | [] -> 0
+  | _ :: t -> 1 + length t
+
+let rec append lst1 lst2 =
+  match lst1 with
+  | [] -> lst2
+  | h :: t -> h :: append t lst2
+
+(*
+  let empty = function
+    | [] -> true
+    | _ :: _ -> false
+*)
+let empty lst =
+  lst = []
+
 type day = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
 
 
--- ocaml_programming/test/test_ocaml_programming.ml
+++ ocaml_programming/test/test_ocaml_programming.ml
@@ -4,6 +4,11 @@ open Ocaml_programming
 let make_sum_test name expected_output input =
   name >:: (fun _ -> assert_equal expected_output (sum input) ~printer:string_of_int)
 
+let make_length_test name expected_output input =
+  name >:: (fun _ -> assert_equal expected_output (length input) ~printer:string_of_int)
+
+let make_empty_test name expected_output input =
+  name >:: (fun _ -> assert_equal expected_output (empty input) ~printer:string_of_bool)
 
 let tests = "test suite for sum" >::: [
   make_sum_test "empty" 0 [];
@@ -12,6 +17,22 @@ let tests = "test suite for sum" >::: [
 ]
 
 let _ = run_test_tt_main tests
+
+let test = "test suite for length" >::: [
+  make_length_test "empty" 0 [];
+  make_length_test "singleton" 1 [1];
+  make_length_test "two_elements" 2 [1; 2];
+]
+
+let _ = run_test_tt_main test
+
+let test = "test suite for empty" >::: [
+  make_empty_test "empty" true [];
+  make_empty_test "singleton" false [1];
+  make_empty_test "two_elements" false [1; 2];
+]
+
+let _ = run_test_tt_main test
 
 let make_next_weekday_test name expected_output input =
   name >:: (fun _ -> assert_equal expected_output (next_weekday input))
